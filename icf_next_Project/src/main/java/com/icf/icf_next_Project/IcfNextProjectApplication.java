9256d41d021c66e48809fff411ef4b3f0d344bee
Rishabh <Rishabh@Thunder>
2021-04-18 13:19:37 +0530

    Project Start

---
+++ icf_next_Project/src/main/java/com/icf/icf_next_Project/IcfNextProjectApplication.java
@@ -0,0 +1,13 @@
+package com.icf.icf_next_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcfNextProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcfNextProjectApplication.class, args);
+	}
+
+}
---
+++ icf_next_Project/src/test/java/com/icf/icf_next_Project/IcfNextProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icf.icf_next_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcfNextProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
