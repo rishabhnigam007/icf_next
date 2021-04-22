92f9cc0dcf758053538a4d9be4259a804d17c228
Rishabh <Rishabh@Thunder>
2021-04-22 23:50:24 +0530

    Spring Security Login Enable...

--- icf_next_Project/src/main/java/com/icf/icf_next_Project/configuration/SecurityConfiguration.java
+++ icf_next_Project/src/main/java/com/icf/icf_next_Project/configuration/SecurityConfiguration.java
@@ -2,11 +2,13 @@ package com.icf.icf_next_Project.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@SuppressWarnings("deprecation")
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 {
@@ -27,5 +29,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 	public PasswordEncoder getPasswordEncoder()
 	{
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception 
+	{
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("ADMIN","USER")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
 	}
 }
